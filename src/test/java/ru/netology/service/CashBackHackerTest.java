2e99e64f68a4e79ea8ca8467e77298ea5572a942
Anastasia <90128050+SantAlice@users.noreply.github.com>
2023-09-16 01:21:10 +0300

    Used junit4, modified test class

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -1,52 +1,52 @@
 package ru.netology.service;
-import org.testng.Assert;
-import ru.netology.service.CashbackHackService;
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
+
+
 
 public class CashBackHackerTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void fullAmountTest() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void boundaryMaxRemainTest() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
         int actual = service.remain(amount);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void equivalentRemainTest() {
         CashbackHackService service = new CashbackHackService();
         int amount = 500;
         int actual = service.remain(amount);
         int expected = 500;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void boundaryMinRemainTest() {
         CashbackHackService service = new CashbackHackService();
         int amount = 100;
         int actual = service.remain(amount);
         int expected = 900;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void boundaryZeroRemainTest() {
         CashbackHackService service = new CashbackHackService();
         int amount = 0;
         int actual = service.remain(amount);
         int expected = 1000;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
